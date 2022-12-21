5797b3dcf780d3104d375ee0ecdb1f5d0bca15e3
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-21 12:15:26 -0500

    sp doesn't have lock creation permission

--- rg.tf
+++ rg.tf
@@ -2,10 +2,3 @@ resource "azurerm_resource_group" "chat-pindropt-fail" {
   name     = "chat.pindropt.fail"
   location = "South Central US"
 }
-
-resource "azurerm_management_lock" "chat-pindropt-fail-rg" {
-  name       = "${azurerm_resource_group.chat-pindropt-fail.name}-lock"
-  scope      = azurerm_resource_group.chat-pindropt-fail.id
-  lock_level = "CanNotDelete"
-  notes      = "Managed by terraform."
-}
